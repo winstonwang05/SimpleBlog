b8f42ae1534531dfaf7c58103b84969982cf356e
winston <1473457657@qq.com>
2025-09-16 19:57:20 +0800

    拦截器请求路径放行

--- src/main/java/com/itheima/config/SecurityConfig.java
+++ src/main/java/com/itheima/config/SecurityConfig.java
@@ -2,7 +2,10 @@ package com.itheima.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * 使用Argon2算法进行加密
@@ -14,5 +17,18 @@ public class SecurityConfig {
     public Argon2PasswordEncoder argon2PasswordEncoder() {
         // 参数: saltLen, hashLen, parallelism, memory, iterations
         return new Argon2PasswordEncoder(16, 32, 1, 1 << 16, 3);
+    }
+    /**
+     * 拦截器放行（放行的路径就在下面的requestMatchers）
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/user/register", "/api/user/login").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
     }
 }
