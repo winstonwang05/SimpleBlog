286e4262b523d0eabb8245b538d0b50491bf257f
winston <1473457657@qq.com>
2025-09-16 17:46:41 +0800

    update

--- src/main/java/com/itheima/service/impl/PasswordEncoder.java
+++ src/main/java/com/itheima/service/impl/PasswordEncoder.java
@@ -8,21 +8,32 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class PasswordEncoder {
-    // 传入的密码
+    // 密码编码器（Argon2算法）
     private final Argon2PasswordEncoder passwordEncoder;
     // 配置的pepper
     @Value("${user.password.pepper}")
     private String pepper;
-    // 构造方法
 
+    // 构造方法
     public PasswordEncoder(Argon2PasswordEncoder passwordEncoder) {
         this.passwordEncoder = passwordEncoder;
     }
-    // 注册时加密
+
+    /**
+     *
+     * @param rawPassword 传入的密码
+     * @return 加密后的密码
+     */
     public String hashPassword(String rawPassword) {
         return passwordEncoder.encode(rawPassword + ":" + pepper);
     }
-    // 登录时检验
+
+    /**
+     *
+     * @param rawPassword 传入的密码
+     * @param storeHash 数据库中的密码
+     * @return 是否匹配
+     */
     public boolean verifyPassword(String rawPassword, String storeHash) {
         return passwordEncoder.matches(rawPassword + ":" + pepper, storeHash);
     }
